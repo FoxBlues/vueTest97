fd05a00cde6a7567cf42b89e5d44af710683a970
FoxBlues <81282064+FoxBlues@users.noreply.github.com>
2021-03-24 14:35:11 +0800

    fox
    2021.3.24

---
+++ src/view/First.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="first-app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "First",
+        data(){
+          return {
+            msg: "welcome to firstApp"
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
